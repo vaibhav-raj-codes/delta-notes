0f7af6d6778acfc5fab62824666b3737a5bbd7c4
= <=>
2025-08-21 21:49:01 +0530

    added part 28 with api request-recieve cycle and how they work with async await functions

--- async,await,api_part_28/script.js
+++ async,await,api_part_28/script.js
@@ -82,5 +82,92 @@ async function setter() {
 setter();
 */
 
+/* line 92
 // # api in JS
 // api stands for application programming interface.
+// api request and recieve data in json format
+// Note: values allowed in json data: object, array, string, number, "true", "false", "null"
+// Note: all keys are strings in json, so to access them we need "keyName"
+// https://jsonlint.com can be used to check json syntax validity
+*/
+
+/* line 104
+// # JSON.parse() method in JS
+// to parse a string data into a JS object
+// json data is sent as a string so we need to parse the data to read it, which can be done using this method
+let jsonResponse = `{"fact":"Approximately 1\/3 of cat owners think their pets are able to read their minds.","length":78}`
+console.log(jsonResponse); // outputs a string and jsonResponse.fact is invalid
+
+// using JSON.parse(data) we can convert the json string to a parsed js object, now we can do thing like parsedData.fact;
+let parsedResponse = JSON.parse(jsonResponse);
+console.log(parsedResponse.fact); 
+*/   
+
+/* line 115
+// # JSON.stringify(json) method in js
+// to parse a JS object data into JSON
+// we use this when making our own APIs
+let student = {name: "dude", age: 69, human: true};
+console.log(student);
+
+let jsonConvert = JSON.stringify(student);
+console.log(jsonConvert); // outputs a string
+*/
+
+// # NOTE: tools for testing API requests- hoppscoth or postman
+
+/* line 138
+// # fetch(url) - javascript method to send API requests
+let url = "https://catfact.ninja/fact";
+// in the networks tab after inspecting we can notice this fetch requests
+// NOTE: fetch(url) returns a promise to us
+fetch(url)
+    .then((res) => {
+        console.log("Success -", res);
+        newPromise = res.json();
+        // this step parses the res.json promise and returns another promise which has the proper json data
+        newPromise
+            .then((data) => {
+                console.log(data.fact); // this will finally print the catfact that the api sent
+            })
+            // here data object has 2 keys fact and length so we used data.fact to access only value inside fact
+    })
+    .catch((err) => {   
+        console.log("ERROR -", err);  
+    });
+*/
+
+/* line 154
+// # shorter hand way of writing the same code above
+let url = "https://catfact.ninja/fact";
+fetch(url)
+    .then((res) => {
+        console.log("Success -", res);
+        return res.json();
+    })
+    .then((data)=> { // this is trggered if above .then() is successful
+        console.log(data.fact);
+    })
+    .catch((err) => {   
+        console.log("ERROR -", err);  
+    });
+*/
+
+/* line 173
+// # using async and await on api request response cycle
+let url = "https://catfact.ninja/fact";
+
+async function getFacts() {
+    try {
+        let res = await fetch(url);
+        console.log(res); // now this line will wait to print res until above line is excecuted
+        let data = await res.json();
+        console.log(data.fact);
+    }
+    catch(err) {
+        console.log("Error -", err);
+    }
+}
+
+getFacts();
+*/
